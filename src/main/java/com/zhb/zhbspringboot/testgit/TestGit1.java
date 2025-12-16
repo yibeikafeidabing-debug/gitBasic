22f0da9b69b9dfa97498ca147ad5a22c153891f3
朱红兵 <1653074120@qq.com>
2025-12-16 21:54:07 +0800

    feat: 第一次提交git ，写了个name字段

---
+++ src/main/java/com/zhb/zhbspringboot/testgit/TestGit1.java
@@ -0,0 +1,13 @@
+package com.zhb.zhbspringboot.testgit;
+
+public class TestGit1 {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
